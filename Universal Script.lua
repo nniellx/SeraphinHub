c02702bda7d1f0e4f42473546eba3d4c2a7592b3
nniellx <nilnilnil276@gmail.com>
2025-09-11 16:49:33 +0800

    Create Universal Script.lua

---
+++ Universal Script.lua
@@ -0,0 +1,9 @@
+-- Universal Script Loader
+local games = {
+    [119048529960596] = "https://raw.githubusercontent.com/nniellx/SeraphinHub/main/RestaurantTycoon3.lua"))()",
+    -- tambahin game lain kalau perlu
+}
+
+local id = game.PlaceId
+local url = games[id] or "https://raw.githubusercontent.com/nniellx/UniversalScript/main/default.lua"
+loadstring(game:HttpGet(url))()
