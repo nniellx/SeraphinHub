f89d8539721e1da0605c8ae3700ce20c6c440c77
nniellx <nilnilnil276@gmail.com>
2025-09-12 21:15:08 +0800

    Update MainHub.lua

--- MainHub.lua
+++ MainHub.lua
@@ -1,9 +1,7 @@
--- Universal Script Loader
 local games = {
     [119048529960596] = "https://raw.githubusercontent.com/nniellx/SeraphinHub/main/RestaurantTycoon3.lua",
-    -- tambahin game lain kalau perlu
 }
 
 local id = game.PlaceId
-local url = games[id] or "https://raw.githubusercontent.com/nniellx/SeraphinHub/refs/heads/main/MainHub.lua"
+local url = games[id] or "https://raw.githubusercontent.com/nniellx/SeraphinHub/main/MainHub.lua"
 loadstring(game:HttpGet(url))()
